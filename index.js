a57506833c6ab87b831431ae7fb98c42ca731c5a
Igor Popov <popov.nirvana@gmail.com>
2025-09-01 08:50:33 +0300

    Change for Windows LLVM 21.1.0

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const path = require('path');
 async function run() {
   try {
     // Change the three following constants when updating LLVM version
-    const downloadUrl = 'https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz';
-    const versionTag = '15.0.6'; // tool-cache expects this to follow semver.org
+    const downloadUrl = 
+    'https://github.com/llvm/llvm-project/releases/download/llvmorg-21.1.0/clang+llvm-21.1.0-x86_64-pc-windows-msvc.tar.xz';
+    const versionTag = '21.1.0'; // tool-cache expects this to follow semver.org
     // CAUTION: semver syntax is a *giant* pain in the butt. For example,
     // "2022.08.0" is not compliant because of a leading zero in "08". When
     // updating this value, you should check in a node shell with this command
